d02973839fd6f2dc8e7b45601cba862cf64e4096
ssfdust <ssfdust@gmail.com>
2023-06-18 21:17:07 +0800

    upload to github

---
+++ README.md
@@ -0,0 +1,3 @@
+Rustup SPEC
+-----------
+The rpm spec for rustup. Convert from archlinux PKGBUILD. Most codes are generated from GTP-4.
---
+++ rustup-profile.sh
@@ -0,0 +1,1 @@
+export PATH=/home/vagrant/.cargo/bin:/home/vagrant/.local/bin:/home/vagrant/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/lib/rustup/bin
